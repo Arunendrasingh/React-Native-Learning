ca0c3d690a824620227b2239d691ada87e651abc
Arunendra Singh <singharunendra978@gmail.com>
2024-10-02 00:44:21 +0530

    Added naviagation and passed the players as the params

--- learningApps/TicTocToe.jsx
+++ learningApps/TicTocToe.jsx
@@ -36,16 +36,8 @@ const getCellStyle = index => {
   return {...baseStyle, ...borderStyle};
 };
 
-const GameStartScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>This is a Game Start Screen</Text>
-    </View>
-  );
-};
-
 // GameHomeScreen component
-const GameHomeScreen = () => {
+const GameHomeScreen = ({navigation}) => {
   const [player1, setPlayer1] = useState('');
   const [player2, setPlayer2] = useState('');
 
@@ -53,17 +45,18 @@ const GameHomeScreen = () => {
 
   // Function to handle the start of the game
   const startGame = () => {
-    // You can implement the logic to start the game here
-    console.log(`Starting game between ${player1} and ${player2}`);
+    navigation.navigate('Game', {players: [player1, player2]});
+    setPlayer1('');
+    setPlayer2('');
   };
 
-  // Function to render each game record
   const renderRecord = ({item}) => (
     <View style={gameHomeStyles.recordRow}>
       <Text style={gameHomeStyles.recordPlayer}>{item.player}</Text>
       <Text
         style={[
           gameHomeStyles.recordStatus,
+          // Set the color based on the status of the game
           item.status === 'Win'
             ? gameHomeStyles.win
             : item.status === 'Lose'
@@ -98,7 +91,13 @@ const GameHomeScreen = () => {
           onChangeText={setPlayer2}
         />
         {/* Start Game Button */}
-        <TouchableOpacity style={gameHomeStyles.button} onPress={startGame}>
+        <TouchableOpacity
+          style={[
+            gameHomeStyles.button,
+            (player1 === '' || player2 === '') && gameHomeStyles.disabledButton,
+          ]}
+          onPress={startGame}
+          disabled={player1 === '' || player2 === ''}>
           <Text style={gameHomeStyles.buttonText}>Start Game</Text>
         </TouchableOpacity>
       </View>
@@ -125,6 +124,238 @@ const GameHomeScreen = () => {
     </View>
   );
 };
+
+const GameBoard = ({route}) => {
+  const initialBoard = () =>
+    Array.from({length: 9}, (_, index) => {
+      return {selected: null, selectedByPlayer: '', position: index + 1};
+    });
+
+  const {players} = route.params;
+  const [board, setBoard] = useState(() => initialBoard());
+  const [playerTurn, setPlayerTurn] = useState(players[0]);
+
+  const [winner, setWinner] = useState(null);
+
+  const resetGame = () => {
+    setBoard(initialBoard());
+    setWinner(null);
+    setPlayerTurn(players[0]);
+  };
+
+  function getWinner(currentBoard) {
+    let winnerName;
+
+    const winningCombinations = [
+      [0, 1, 2], // Row 1
+      [3, 4, 5], // Row 2
+      [6, 7, 8], // Row 3
+      [0, 3, 6], // Column 1
+      [1, 4, 7], // Column 2
+      [2, 5, 8], // Column 3
+      [0, 4, 8], // Diagonal
+      [2, 4, 6], // Diagonal
+    ];
+
+    for (const [a, b, c] of winningCombinations) {
+      const player = currentBoard[a].selectedByPlayer;
+      // Check that the selectedByPlayer is not empty and matches in all three positions
+      if (
+        player &&
+        player === currentBoard[b].selectedByPlayer &&
+        player === currentBoard[c].selectedByPlayer
+      ) {
+        winnerName = player;
+        break;
+      }
+    }
+
+    return winnerName;
+  }
+
+  const setSelectedPlayOnBoard = (selected, currentPlayer, selectedIndex) => {
+    // New board
+
+    if (selected != null) {
+      return;
+    }
+
+    if (winner != null) {
+      return;
+    }
+
+    let newBoard = board.map((item, index) => {
+      if (currentPlayer === players[0] && selectedIndex === index) {
+        return {...item, selected: 0, selectedByPlayer: currentPlayer};
+      } else if (selectedIndex === index) {
+        return {...item, selected: 1, selectedByPlayer: currentPlayer};
+      }
+      return item;
+    });
+
+    setBoard(newBoard);
+    // Check the
+
+    let currentWinner = getWinner(newBoard);
+    console.log('Current Winner of the game', currentWinner);
+
+    if (currentWinner) {
+      setWinner(currentWinner);
+      return;
+    }
+
+    if (currentPlayer === players[0]) {
+      setPlayerTurn(players[1]);
+    } else {
+      setPlayerTurn(players[0]);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.gameContainer}>
+        <Text style={styles.text}>
+          {winner === null ? `${playerTurn}'s turn` : `${winner} win the game.`}
+        </Text>
+        <View style={styles.boardContainer}>
+          {board.map(({selected, position}, index) => (
+            <BoardIcon
+              key={position}
+              index={index}
+              selected={selected}
+              setSelected={setSelectedPlayOnBoard}
+              playerTurn={playerTurn}
+            />
+          ))}
+        </View>
+        <TouchableOpacity style={styles.resetButton} onPress={resetGame}>
+          <Text style={styles.buttonText}>Reset the Game</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+function BoardIcon({index, selected, setSelected, playerTurn}) {
+  const iconToggle = () => {
+    console.log('Icons is selected at index: ', index, playerTurn);
+    setSelected(selected, playerTurn, index);
+  };
+
+  const getIcon = () => {
+    if (selected === null) {
+      return (
+        <FontAwesomeIcon icon={faPencil} size={35} style={styleIcon.pencil} />
+      );
+    } else if (selected === 0) {
+      return (
+        <FontAwesomeIcon
+          icon={faCircle}
+          size={40}
+          style={styleIcon.circleXMark}
+        />
+      );
+    } else {
+      return (
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          size={40}
+          style={styleIcon.circle}
+        />
+      );
+    }
+  };
+  return (
+    <View style={getCellStyle(index)}>
+      <Pressable onPress={iconToggle}>{getIcon()}</Pressable>
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
+
+const TicTocToe = () => {
+  return (
+    <Stack.Navigator headerTitleAlign="center">
+      <Stack.Screen
+        name="Home"
+        component={GameHomeScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Game"
+        component={GameBoard}
+        options={{
+          headerTransparent: true,
+          headerBlurEffect: 'dark',
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: '#fff',
+          },
+          headerBackTitleStyle: {
+            color: '#fff',
+            fontSize: 16,
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#183153',
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  gameContainer: {
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  boardContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '90%',
+    aspectRatio: 1,
+    borderColor: 'black',
+    alignSelf: 'center',
+  },
+  boardItem: {
+    width: '33.33%',
+    height: '33.33%',
+    borderColor: 'gray',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  resetButton: {
+    width: 200,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FF6F61',
+    borderRadius: 10,
+  },
+  text: {
+    color: '#ffffff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
+
+const styleIcon = StyleSheet.create({
+  pencil: {
+    color: '#FFD43B',
+  },
+  circle: {
+    color: '#63E6BE',
+  },
+  circleXMark: {color: '#B197FC'},
+});
 
 const gameHomeStyles = StyleSheet.create({
   container: {
@@ -208,265 +439,11 @@ const gameHomeStyles = StyleSheet.create({
   scrollViewContent: {
     paddingBottom: 10,
   },
+  disabledButton: {
+    backgroundColor: '#888', // Greyed-out color for disabled button
+    opacity: 0.6, // Lower opacity to indicate disabled state
+  },
 });
-
-/**
- * Tic Tac Toe game component.
- *
- * @return {JSX.Element} Tic Tac Toe game component.
- */
-const GameBoard = () => {
-  /**
-   * Creates an initial game board with 9 empty positions.
-   *
-   * @return {Array} An array of objects representing the game board positions.
-   */
-  const initialBoard = () =>
-    Array.from({length: 9}, (_, index) => {
-      return {selected: null, selectedByPlayer: '', position: index + 1};
-    });
-
-  /**
-   * The two players in the game.
-   */
-  const players = ['Tom', 'Jerry'];
-
-  /**
-   * The current game board.
-   */
-  const [board, setBoard] = useState(() => initialBoard());
-  /**
-   * The player who is currently playing.
-   */
-  const [playerTurn, setPlayerTurn] = useState(players[0]);
-
-  /**
-   * The winner of the game.
-   */
-  const [winner, setWinner] = useState(null);
-
-  /**
-   * Resets the game to the initial state.
-   */
-  const resetGame = () => {
-    setBoard(initialBoard());
-    setWinner(null);
-    setPlayerTurn(players[0]);
-  };
-
-  /**
-   * Finds the winner of the game based on the current board.
-   *
-   * @param {Array} currentBoard The current game board.
-   * @return {String} The winner of the game or null if there is no winner.
-   */
-  function getWinner(currentBoard) {
-    let winnerName;
-
-    const winningCombinations = [
-      [0, 1, 2], // Row 1
-      [3, 4, 5], // Row 2
-      [6, 7, 8], // Row 3
-      [0, 3, 6], // Column 1
-      [1, 4, 7], // Column 2
-      [2, 5, 8], // Column 3
-      [0, 4, 8], // Diagonal
-      [2, 4, 6], // Diagonal
-    ];
-
-    for (const [a, b, c] of winningCombinations) {
-      const player = currentBoard[a].selectedByPlayer;
-      // Check that the selectedByPlayer is not empty and matches in all three positions
-      if (
-        player &&
-        player === currentBoard[b].selectedByPlayer &&
-        player === currentBoard[c].selectedByPlayer
-      ) {
-        winnerName = player;
-        break;
-      }
-    }
-
-    return winnerName;
-  }
-
-  /**
-   * Sets the selected play on the game board.
-   *
-   * @param {Number} selected The selected play (0 for player 1, 1 for player 2, null for empty).
-   * @param {String} currentPlayer The current player.
-   * @param {Number} selectedIndex The index of the selected play on the board.
-   */
-  const setSelectedPlayOnBoard = (selected, currentPlayer, selectedIndex) => {
-    // New board
-
-    if (selected != null) {
-      return;
-    }
-
-    if (winner != null) {
-      return;
-    }
-
-    let newBoard = board.map((item, index) => {
-      if (currentPlayer === players[0] && selectedIndex === index) {
-        return {...item, selected: 0, selectedByPlayer: currentPlayer};
-      } else if (selectedIndex === index) {
-        return {...item, selected: 1, selectedByPlayer: currentPlayer};
-      }
-      return item;
-    });
-
-    setBoard(newBoard);
-    // Check the
-
-    let currentWinner = getWinner(newBoard);
-    console.log('Current Winner of the game', currentWinner);
-
-    if (currentWinner) {
-      setWinner(currentWinner);
-      return;
-    }
-
-    if (currentPlayer === players[0]) {
-      setPlayerTurn(players[1]);
-    } else {
-      setPlayerTurn(players[0]);
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text>This is a Header Text</Text>
-      </View>
-      <View style={styles.gameContainer}>
-        <Text style={styles.text}>
-          {winner === null ? `${playerTurn}'s turn` : `${winner} win the game.`}
-        </Text>
-        <View style={styles.boardContainer}>
-          {board.map(({selected, position}, index) => (
-            <BoardIcon
-              key={position}
-              index={index}
-              selected={selected}
-              setSelected={setSelectedPlayOnBoard}
-              playerTurn={playerTurn}
-            />
-          ))}
-        </View>
-        <TouchableOpacity style={styles.resetButton} onPress={resetGame}>
-          <Text style={styles.buttonText}>Reset the Game</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-};
-
-function BoardIcon({index, selected, setSelected, playerTurn}) {
-  const iconToggle = () => {
-    console.log('Icons is selected at index: ', index, playerTurn);
-    setSelected(selected, playerTurn, index);
-  };
-
-  const getIcon = () => {
-    if (selected === null) {
-      return (
-        <FontAwesomeIcon icon={faPencil} size={35} style={styleIcon.pencil} />
-      );
-    } else if (selected === 0) {
-      return (
-        <FontAwesomeIcon
-          icon={faCircle}
-          size={40}
-          style={styleIcon.circleXMark}
-        />
-      );
-    } else {
-      return (
-        <FontAwesomeIcon
-          icon={faCircleXmark}
-          size={40}
-          style={styleIcon.circle}
-        />
-      );
-    }
-  };
-  return (
-    <View style={getCellStyle(index)}>
-      <Pressable onPress={iconToggle}>{getIcon()}</Pressable>
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
-
-const TicTocToe = () => {
-  return (
-    <Stack.Navigator headerTitleAlign="center">
-      <Stack.Screen
-        name="Home"
-        component={GameHomeScreen}
-        options={{headerShown: false}}
-      />
-    </Stack.Navigator>
-  );
-};
 
 export default TicTocToe;
-export {GameStartScreen, GameBoard};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#183153',
-  },
-  buttonText: {
-    color: '#ffffff',
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-  gameContainer: {
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  boardContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    width: '90%',
-    aspectRatio: 1,
-    borderColor: 'black',
-    alignSelf: 'center',
-  },
-  boardItem: {
-    width: '33.33%',
-    height: '33.33%',
-    borderColor: 'gray',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  resetButton: {
-    width: 200,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FF6F61',
-    borderRadius: 10,
-  },
-  text: {
-    color: '#ffffff',
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-});
-
-const styleIcon = StyleSheet.create({
-  pencil: {
-    color: '#FFD43B',
-  },
-  circle: {
-    color: '#63E6BE',
-  },
-  circleXMark: {color: '#B197FC'},
-});
+export {GameBoard};
